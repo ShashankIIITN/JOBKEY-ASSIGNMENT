63825ff80d0683c985bbb0bb50a94ace1ed45319
ShashankIIITN <shashanktripathi.st15a@gmail.com>
2024-10-07 21:47:26 +0530

    added login signup form

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -1,42 +1,57 @@
 import React, { useState } from "react";
+import { FormField } from "./SignUp";
 
 function LogIn() {
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
+	const [formData, setformData] = useState({
+		email: "",
+		password: "",
+	});
 
+	const updateFormdata = (e) => {
+		setformData({ ...formData, [e.target.name]: e.target.value });
+	};
 	const handleLogin = (e) => {
 		e.preventDefault();
 		console.log("Logging in:", { email, password });
 
-		setEmail("");
-		setPassword("");
+		setformData({
+			email: "",
+			password: "",
+		});
 	};
 
 	return (
-		<div className="login-container flex flex-col justify-center items-center h-full border">
-			<h2>Login</h2>
-			<form onSubmit={handleLogin}>
-				<div>
-					<label>Email:</label>
-					<input
-						type="email"
-						value={email}
-						onChange={(e) => setEmail(e.target.value)}
-						placeholder="Enter your email"
-						required={true}
-					/>
-				</div>
-				<div>
-					<label>Password:</label>
-					<input
-						type="password"
-						value={password}
-						onChange={(e) => setPassword(e.target.value)}
-						placeholder="Enter your password"
-						required={true}
-					/>
-				</div>
-				<button type="submit">Login</button>
+		<div className="login-container flex flex-col justify-center items-center h-full border gap-10">
+			<h1 className="text-4xl">Login</h1>
+			<form
+				onSubmit={handleLogin}
+				className="text-2xl flex flex-col gap-5 w-[450px] border border-blue-700 p-5"
+			>
+				<FormField
+					type="email"
+					name="email"
+					formData={formData}
+					onChange={updateFormdata}
+					placeholder="Enter your email"
+					required={true}
+					label="Email"
+				/>
+				<FormField
+					type="password"
+					name="password"
+					formData={formData}
+					onChange={updateFormdata}
+					placeholder="Enter your password"
+					required={true}
+					label="Password"
+				/>
+
+				<button
+					type="submit"
+					className="border border-black w-1/3 self-center bg-blue-400 pb-1 hover:bg-blue-200"
+				>
+					Login
+				</button>
 			</form>
 		</div>
 	);
--- frontend/src/components/auth/SignUp.jsx
+++ frontend/src/components/auth/SignUp.jsx
@@ -1,9 +1,102 @@
-import React from 'react'
+import React, { useState } from "react";
+
+export const FormField = ({
+	type,
+	name,
+	formData,
+	onChange,
+	required,
+	placeholder,
+	className,
+	label,
+}) => {
+	return (
+		<div className="flex justify-between items-center ">
+			<label>{label}:</label>
+			<input
+				type={type}
+				name={name}
+				className={" outline-none border border-black " + className}
+				value={formData[name]}
+				onChange={onChange}
+				placeholder={placeholder}
+				required={required}
+			/>
+		</div>
+	);
+};
 
 function SignUp() {
-  return (
-    <div>SignUp</div>
-  )
+	const [formData, setformData] = useState({
+		name: "",
+		email: "",
+		password: "",
+		confirmPassword: "",
+	});
+
+	const updateFormdata = (e) => {
+		setformData({ ...formData, [e.target.name]: e.target.value });
+	};
+
+	const handleLogin = (e) => {
+		e.preventDefault();
+		console.log("Logging in:", { email, password });
+
+		setEmail("");
+		setPassword("");
+	};
+	return (
+		<div className="login-container flex flex-col justify-center items-center h-full border gap-10">
+			<h1 className="text-4xl">Login</h1>
+			<form
+				onSubmit={handleLogin}
+				className="text-2xl flex flex-col gap-5 w-[450px] border border-blue-700 p-5"
+			>
+				<FormField
+					name="name"
+					type="text"
+					formData={formData}
+					onChange={updateFormdata}
+					placeholder="Enter your name"
+					required={true}
+					label="Name"
+				/>
+				<FormField
+					type="email"
+					name="email"
+					formData={formData}
+					onChange={updateFormdata}
+					placeholder="Enter your email"
+					required={true}
+					label="Email"
+				/>
+				<FormField
+					type="password"
+					name="password"
+					formData={formData}
+					onChange={updateFormdata}
+					placeholder="Enter new password"
+					required={true}
+					label="Password"
+				/>
+				<FormField
+					type="password"
+					name="confirmPassword"
+					formData={formData}
+					onChange={updateFormdata}
+					placeholder="Re enter your password"
+					required={true}
+					label="Confirm Password"
+				/>
+				<button
+					type="submit"
+					className="border border-black w-1/3 self-center bg-blue-400 pb-1 hover:bg-blue-200"
+				>
+					SignUp
+				</button>
+			</form>
+		</div>
+	);
 }
 
-export default SignUp
+export default SignUp;
