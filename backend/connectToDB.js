a71b01d9ac4adfee0b3314838fd888d0490dfe80
ShashankIIITN <shashanktripathi.st15a@gmail.com>
2024-10-09 00:35:05 +0530

    server db

--- backend/connectToDB.js
+++ backend/connectToDB.js
@@ -1,4 +1,14 @@
 import pg from "pg";
+import fs from "fs";
+
+const getValue = () => {
+	if (process.env.DB_HOST === "localhost") return false;
+	else
+		return {
+			rejectUnauthorized: true,
+			ca: fs.readFileSync("./ca.pem").toString(),
+		};
+};
 
 export const connectTopgDB = () => {
 	const pool = new pg.Pool({
@@ -7,6 +17,7 @@ export const connectTopgDB = () => {
 		database: process.env.DB_NAME,
 		password: process.env.DB_PASSWORD,
 		port: process.env.DB_PORT,
+		ssl: getValue(),
 	});
 
 	pool.connect((err, client, release) => {
